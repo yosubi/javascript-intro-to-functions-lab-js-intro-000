c418f4b7c1a9bfe58b8c541015d7160e27269938
Joseph Moon <nakkta@gmail.com>
2019-09-09 19:44:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function sayHiToGrandma(string){
  if (string.toUpperCase() === string) {
    return "YES INDEED!"
  }
- if (string === "I love you,too") {
-   return "I love you, Grandma"
+ if (string === "I love you, Grandma") {
+   return "I love you, too"
  }
 
 }

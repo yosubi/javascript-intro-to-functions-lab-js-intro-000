6017e49347fd14abea5405ef8436a218a4152ac6
Joseph Moon <nakkta@gmail.com>
2019-09-09 19:43:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function logWhisper(string){
 
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase === string){
+  if (string.toLowerCase() === string){
     return "I can't hear you!" ;
 }
- if (string.toUpperCase === string) {
+ if (string.toUpperCase() === string) {
    return "YES INDEED!"
  }
 
